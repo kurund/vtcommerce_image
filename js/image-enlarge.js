2ca102e6e5e27cd32b90c92d1aad16a2afbcf3d2
victheme <duckzlandd@yahoo.com>
2012-09-13 20:35:35 +0800

    fix hover zoom

--- js/image-enlarge.js
+++ js/image-enlarge.js
@@ -1,3 +1,14 @@
+/**
+ * Image zooming jQuery plugin script by VicTheme.com
+ * 
+ * Available options
+ * (int) multiple = the factor that the image should be enlarged when hovered
+ * (int) mouseInSpeed = the animation speed when zoomed in
+ * (int) mouseOutSpeed = the animation speed when zoomed out
+ * (int) zindex = the z-index to use.
+ * (int) topOffset = set additional top offset
+ * (int) leftOffset = set additional left offset
+ */
 (function ($) {
     $.fn.imageEnlarge = function (options) {
 			//Set the default values, use comma to separate the settings, example:
@@ -8,25 +19,30 @@
 				zindex : 600,
 				topOffset : 0,
 				leftOffset : 0
-			}
+			};
 				
 			var options =  $.extend(defaults, options);
 
 			return this.each(function() {
-				var $items = $(this).find('img'),
-						$position = $items.position(),
-						$iH = $items.outerHeight(true),
-						$iW = $items.outerWidth(true),
+			  
+			  var $items = $(this).find('img');
+			  
+	       // Get the original image height and width only once
+        if ($iH === undefined && $iW === undefined) {
+          var $iH = $items.height(),
+              $iW = $items.width();
+        }
+
+				var $position = $items.position(),
 						iHH = $iH * options.multiple,
 						iHW = $iW * options.multiple,
 						mT = ( iHH - $iH ) / 2,
 						mL = ( iHW - $iW ) / 2,
 						pT = $position.top - mT,
 						pL = $position.left - mL;
+				
 			$(this).hover(function() {
 				$items.addClass('iehover').css({'z-index' : options.zindex, 'position' : 'absolute'}).stop().animate({
-					//marginTop: '-' + mT + 'px',
-					//marginLeft: '-' + mL + 'px',
 					top: pT + parseInt(options.topOffset),
 					left: pL + parseInt(options.leftOffset),
 					width: iHW,
@@ -34,8 +50,6 @@
 				}, options.mouseInSpeed);
 			}, function() {
 				$items.removeClass('iehover').css({'z-index' : options.zindex, 'position' : 'static'}).stop().animate({
-					//marginTop: '0',
-					//marginLeft: '0',
 					top: $position.top,
 					left: $position.left,
 					width: $iW,
